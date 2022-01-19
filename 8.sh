75204d6e35213196a2f5e64dca0ab31e96460b47
RarogCmex <denis7774@gmail.com>
2022-01-19 18:37:53 +0500

    8.sh

---
+++ 8.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ROLLS_MAX=700
+
+ones=0
+twos=0
+threes=0
+fours=0
+fives=0
+sixes=0
+roll=0
+
+while [ "$roll" -lt "$ROLLS_MAX" ]; do
+    let "current_res = $RANDOM % 6"
+    case $current_res in 
+        0) let "ones += 1" ;;
+        1) let "twos += 1" ;;
+        2) let "threes += 1" ;;
+        3) let "fours += 1" ;;
+        4) let "fives += 1" ;;
+        5) let "sixes += 1" ;;
+        esac
+    let "roll += 1"
+    done
+echo "единиц   = $ones"
+echo "двоек    = $twos"
+echo "троек    = $threes"
+echo "четверок = $fours"
+echo "пятерок  = $fives"
+echo "шестерок = $sixes"
