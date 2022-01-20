87eb34467802df35c3c50852052fcf9933158d41
RarogCmex <denis7774@gmail.com>
2022-01-20 11:27:21 +0500

    Fix 8.sh codestyle

--- Entry Level (1-8)/8.sh
+++ Entry Level (1-8)/8.sh
@@ -14,16 +14,16 @@ roll=0
 
 while [ "$roll" -lt "$ROLLS_MAX" ]; do
   let "current_res = $RANDOM % 6"
-    case $current_res in 
-      0) let "ones += 1" ;;
-      1) let "twos += 1" ;;
-      2) let "threes += 1" ;;
-      3) let "fours += 1" ;;
-      4) let "fives += 1" ;;
-      5) let "sixes += 1" ;;
-      esac
-    let "roll += 1"
-  done
+  case $current_res in 
+    0) let "ones += 1" ;;
+    1) let "twos += 1" ;;
+    2) let "threes += 1" ;;
+    3) let "fours += 1" ;;
+    4) let "fives += 1" ;;
+    5) let "sixes += 1" ;;
+    esac
+  let "roll += 1"
+done
 echo "единиц   = $ones"
 echo "двоек    = $twos"
 echo "троек    = $threes"
