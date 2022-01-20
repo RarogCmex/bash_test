28f3852c7c3614bb8f09bf735ea44154fed8a53c
RarogCmex <denis7774@gmail.com>
2022-01-20 11:10:39 +0500

    Fixed 1.sh codestyle

--- Entry Level (1-8)/1.sh
+++ Entry Level (1-8)/1.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+# 
+# Check whether we works as root or non-root user
 
-if [[ `id -ur` == 0 ]]; then
+if [[ `id -u` == 0 ]]; then
 	echo "You work as root"
 else
 	echo "You work as Non-root"
 fi
 
+# Eng:
+# id -u checks the effective user,
+# thus, the launch with setuid is taken into account.
+# If you need only the owner of the process to be accounted,
+# then you need to compare via id -ur
+# Russian:
 # id -u проверяет эффективного пользователя,
 # таким образом учитывается запуск с setuid.
 # Если нужно учитывать только запуск владельца процесса,
