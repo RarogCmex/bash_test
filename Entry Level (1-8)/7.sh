dce9b04a97949c539f7a16488185887b2ecea343
RarogCmex <denis7774@gmail.com>
2022-01-20 11:26:16 +0500

    7.sh and 8.sh codestyle

--- Entry Level (1-8)/7.sh
+++ Entry Level (1-8)/7.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+#
+# Convert meters to miles
 
+# Check whether parameter exist
 if [ -z "$1" ]; then
   echo "You should run that script with numberic parameter"
   exit 1
 fi
 
 echo "scale=3;$1*0.00062" | bc
-
--- Entry Level (1-8)/8.sh
+++ Entry Level (1-8)/8.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#
+# Simulates 700 (ROLLS_MAX) dice rolls, summarize statistics
+# Russian-language output
 
 ROLLS_MAX=700
-
 ones=0
 twos=0
 threes=0
@@ -11,17 +13,17 @@ sixes=0
 roll=0
 
 while [ "$roll" -lt "$ROLLS_MAX" ]; do
-    let "current_res = $RANDOM % 6"
+  let "current_res = $RANDOM % 6"
     case $current_res in 
-        0) let "ones += 1" ;;
-        1) let "twos += 1" ;;
-        2) let "threes += 1" ;;
-        3) let "fours += 1" ;;
-        4) let "fives += 1" ;;
-        5) let "sixes += 1" ;;
-        esac
+      0) let "ones += 1" ;;
+      1) let "twos += 1" ;;
+      2) let "threes += 1" ;;
+      3) let "fours += 1" ;;
+      4) let "fives += 1" ;;
+      5) let "sixes += 1" ;;
+      esac
     let "roll += 1"
-    done
+  done
 echo "единиц   = $ones"
 echo "двоек    = $twos"
 echo "троек    = $threes"
