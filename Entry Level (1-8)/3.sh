ef4bc911ac9473a4a9cce95948376a954074c911
RarogCmex <denis7774@gmail.com>
2022-01-20 11:15:35 +0500

    Improved 3.sh codestyle

--- Entry Level (1-8)/3.sh
+++ Entry Level (1-8)/3.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+#
+# Read single-line reply from commandline and mirror it,
+# But you limited only for 5 second.
 
-
+# Timeout function used to set timeout. 
+# Assumes to be posix-compatible solution
 timeout_function() {
 	sleep 5
 	echo "You didn't have time to promt"
-	kill $$
+	kill $$ #ToDo: graceful exit
 }
 
-timeout_function&
+timeout_function& # Run timeout in background job
 read REPLY
 echo ${REPLY}
-kill $!
+kill $! # Kill timeout function
 
